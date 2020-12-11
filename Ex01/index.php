6c88565e85412a9dd6577f7b1877d305c42a61dc
kevinmilet <kevin.milet@gmail.com>
2020-12-11 09:19:31 +0100

    first commit

---
+++ Ex01/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Partie 8 - Exercice 1</title>
+</head>
+<body>
+    
+    <p><strong>Nom de l'user agent: </strong><?=$_SERVER['HTTP_USER_AGENT'];?></p>
+    <p><strong>Adresse IP: </strong><?=$_SERVER['SERVER_ADDR'];?></p>
+    <p><strong>Nom du serveur: </strong><?=$_SERVER['SERVER_NAME'];?></p>
+
+
+</body>
+</html>
